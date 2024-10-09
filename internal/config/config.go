d4bd67c8de23ffdd1d6701922b288f96a1b616ae
Dhairya Khanna <dk9582671597@gmail.com>
2024-10-09 22:32:40 +0530

    fix: formatting the code

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,29 +6,28 @@ import (
 )
 
 type Config struct {
-	DbURL string `json:"db_url"`
+	DbURL           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
 }
 
 const configFileName = ".gatorconfig.json"
 
-func Read() (Config,error) {
+func Read() (Config, error) {
 	homeDir, err := os.UserHomeDir()
-	if err!=nil{
-		return Config{},err
-	}
-	 filePath := homeDir + "/" + configFileName
-	 fileContent,err := os.ReadFile(filePath)
-	 if err != nil {
+	if err != nil {
 		return Config{}, err
-	 }
-	 config:=Config{}
-	 if err:=json.Unmarshal(fileContent,&config);err!=nil{
-		return Config{},err
+	}
+	filePath := homeDir + "/" + configFileName
+	fileContent, err := os.ReadFile(filePath)
+	if err != nil {
+		return Config{}, err
+	}
+	config := Config{}
+	if err := json.Unmarshal(fileContent, &config); err != nil {
+		return Config{}, err
 
-	 }
-	 return config,err
-
+	}
+	return config, err
 
 }
 func (c *Config) SetUser(userName string) error {
@@ -36,17 +35,17 @@ func (c *Config) SetUser(userName string) error {
 
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-			return err
+		return err
 	}
 
 	filePath := homeDir + "/" + configFileName
 	fileContent, err := json.MarshalIndent(c, "", "  ")
 	if err != nil {
-			return err
+		return err
 	}
 
 	if err := os.WriteFile(filePath, fileContent, 0644); err != nil {
-			return err
+		return err
 	}
 
 	return nil
--- main.go
+++ main.go
@@ -6,18 +6,17 @@ import (
 
 	config "github.com/Dhairya3124/blog-aggregator/internal/config"
 )
-func main(){
-	 configDetails,err:=config.Read()
-	 if err != nil {
-		 log.Fatalf(" Following Error %v",err)
-	 }
-	 configDetails.SetUser("Dhairya")
-	 configDetails,err=config.Read()
-	 if err != nil {
-		log.Fatalf(" Following Error %v",err)
+
+func main() {
+	configDetails, err := config.Read()
+	if err != nil {
+		log.Fatalf(" Following Error %v", err)
+	}
+	configDetails.SetUser("Dhairya")
+	configDetails, err = config.Read()
+	if err != nil {
+		log.Fatalf(" Following Error %v", err)
 	}
 	fmt.Println(configDetails)
-	 
-
 
 }
