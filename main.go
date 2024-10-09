7538d997a3c1c6a44b19ed3fc97978e375262fa7
Dhairya3124 <dhairya3124@gmail.com>
2024-10-09 22:29:30 +0530

    feat: Completed the r/w of the config file

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	config "github.com/Dhairya3124/blog-aggregator/internal/config"
+)
+func main(){
+	 configDetails,err:=config.Read()
+	 if err != nil {
+		 log.Fatalf(" Following Error %v",err)
+	 }
+	 configDetails.SetUser("Dhairya")
+	 configDetails,err=config.Read()
+	 if err != nil {
+		log.Fatalf(" Following Error %v",err)
+	}
+	fmt.Println(configDetails)
+	 
+
+
+}
