0bf0ce8c7a101b6794a929e181cc8fc6d0f40e8c
Dhairya Khanna <dk9582671597@gmail.com>
2024-10-19 13:50:43 +0530

    feat: Added command for creating follow to the feeds

---
+++ sql/queries/follows.sql
@@ -0,0 +1,15 @@
+-- name: CreateFeedFollow :one
+WITH follow AS (
+  INSERT INTO feed_follows(id,created_at,updated_at,feed_id,user_id)
+  VALUES(
+    $1,
+    $2,
+    $3,
+    $4,
+    $5
+  )
+  RETURNING *
+)
+SELECT follow.*, f.name as feed_name, u.name as user_name FROM follow 
+JOIN feeds f ON follow.feed_id = f.id
+JOIN users u ON follow.user_id = u.id;
